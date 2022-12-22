32044e7583e9f7d85b869e16fa46f45dce1fd49c
Feiyi Wang <fwang2@gmail.com>
2022-12-22 10:43:54 -0500

    make it cpu capatible

--- ddp/single_gpu.py
+++ ddp/single_gpu.py
@@ -77,6 +77,18 @@ if __name__ == "__main__":
     parser.add_argument('save_every', type=int, help='How often to save a snapshot')
     parser.add_argument('--batch_size', default=32, type=int, help='Input batch size on each device (default: 32)')
     args = parser.parse_args()
+
+    # setting device on GPU if available, else CPU
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print('Using device:', device)
+    print()
+
+    #Additional Info when using cuda
+    if device.type == 'cuda':
+        print(torch.cuda.get_device_name(0))
+        print('Memory Usage:')
+        print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
+        print('Cached:   ', round(torch.cuda.memory_reserved(0)/1024**3,1), 'GB')
     
-    device = 0  # shorthand for cuda:0
+    # device = 0  # shorthand for cuda:0
     main(device, args.total_epochs, args.save_every, args.batch_size)
